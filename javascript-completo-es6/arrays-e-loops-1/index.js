bb18149e04936777b398088dc8a8d9428fbd9498
yasminconc <yasminsilvaconc@gmail.com>
2023-02-10 11:09:19 -0300

    arrays-e-loops-1

---
+++ javascript-completo-es6/arrays-e-loops-1/index.js
@@ -0,0 +1,34 @@
+
+const videoGames = ['Switch', 'PS4', 'XBox'];
+
+videoGames[0] // Switch
+videoGames[2] // Xbox
+
+//
+
+const ultimoItem = videoGames.pop(); // Remove o último item e retorna ele
+
+const adicionaFinal = videoGames.push('3DS'); // Adiciona ao final da array
+
+videoGames.length; // 3
+
+//
+
+
+for ( let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+// Retorna de 0 a 9 no console
+
+
+//
+
+let i = 0;
+
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+// Retorna de 0 a 9 no console
+
+
