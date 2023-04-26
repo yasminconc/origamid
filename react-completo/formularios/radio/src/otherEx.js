74f7e63fb5ca0e61e1ce691c1c9c3d1fbe59d481
yasminconc <yasminsilvaconc@gmail.com>
2023-04-26 11:43:33 -0300

    radio

---
+++ react-completo/formularios/radio/src/otherEx.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const otherEx = () => {
+
+    const [produto, setProduto] = React.useState('');
+    const [cor, setCor] = React.useState('');
+  
+    return (
+      <form>
+        <h2>Dispositivo</h2>
+        <label>
+          <input
+            type="radio"
+            value="notebook"
+            checked={produto === 'notebook'}
+            onChange={({ target }) => setProduto(target.value)}
+          />
+          Notebook
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="smartphone"
+            checked={produto === 'smartphone'}
+            onChange={({ target }) => setProduto(target.value)}
+          />
+          Smartphone
+        </label>
+  
+        <h2>Cor</h2>
+        <label>
+          <input
+            type="radio"
+            value="azul"
+            checked={cor === 'azul'}
+            onChange={({ target }) => setCor(target.value)}
+          />
+          Azul
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="vermelho"
+            checked={cor === 'vermelho'}
+            onChange={({ target }) => setCor(target.value)}
+          />
+          Vermelho
+        </label>
+      </form>
+    );
+  };
+  
+export default otherEx
